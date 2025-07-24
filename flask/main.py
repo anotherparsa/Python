25da2a849bd1621ee9e9061ee8b0dd3059f706f1
anotherparsa <anotherparsa@gmail.com>
2025-07-24 07:11:00 +0330

    passing data via dictionary

--- flask/main.py
+++ flask/main.py
@@ -10,6 +10,15 @@ def home_page():
 def aboutus_page():
     return render_template("aboutus.html")
 
+kwargs = {
+    "arg1" : "test1",
+    "arg2" : "test2",
+    "arg3" : "test3",
+    "arg4" : "test4",
+    "arg5" : "test5",
+    "arg6" : "test6",
+}
+
 @app.route("/data")
 def passingdata_page():
-    return render_template("data.html", name="Test")
+    return render_template("data.html", **kwargs)
