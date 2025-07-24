43eee64a207ec701d82038c6a60ab359f90839bc
anotherparsa <anotherparsa@gmail.com>
2025-07-24 11:45:03 +0330

    first endpoint of API

--- flask/main.py
+++ flask/main.py
@@ -1,24 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home_page():
-    return render_template("home.html")
+stores = [
+    {
+        "name" : "my store",
+        "items" : [
+            {
+                "name" : "chair",
+                "price" : 10
+            }
+        ]
+    }
+]
 
-@app.route("/about")
-def aboutus_page():
-    return render_template("aboutus.html")
-
-kwargs = {
-    "arg1" : "test1",
-    "arg2" : "test2",
-    "arg3" : "test3",
-    "arg4" : "test4",
-    "arg5" : "test5",
-    "arg6" : "test6",
-}
-
-@app.route("/data")
-def passingdata_page():
-    return render_template("data.html", **kwargs)
+@app.get("/stores")
+def get_stores():
+    return {"stores" : stores}
